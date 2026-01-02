5477cd39df208f5745c982120d57ef5680eb7d6b
ashtonite1 <ashtonrknight@gmail.com>
2026-01-02 15:24:29 -0500

    update to ischanging

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,7 +67,9 @@ repositories {
 }
 
 dependencies {
-    implementation("xyz.zephr.sdk.ashtonite1:positioning:0.3.+")
+    implementation("xyz.zephr.sdk.ashtonite1:positioning:0.3.+") {
+        isChanging = true
+    }
     implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.1")
     implementation("androidx.activity:activity-compose:1.10.1")
