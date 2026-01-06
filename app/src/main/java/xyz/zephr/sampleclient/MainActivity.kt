a380ef351c3f25181e37f1778f6f8c62b7763247
ashtonite1 <ashtonrknight@gmail.com>
2026-01-06 13:43:49 -0500

    organize and clean up for v3

--- app/src/main/java/xyz/zephr/sampleclient/MainActivity.kt
+++ app/src/main/java/xyz/zephr/sampleclient/MainActivity.kt
@@ -1,44 +1,74 @@
 package xyz.zephr.sampleclient
 
 import android.Manifest
-import android.content.pm.PackageManager
+import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.annotation.RequiresPermission
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import xyz.zephr.sampleclient.ui.theme.ZephrSampleClientAppTheme
 import xyz.zephr.sdk.api.ZephrLocationManager
 import xyz.zephr.sdk.api.ZephrTypes
 
+private const val TAG = "ZephrSampleClientApp"
+
 class MainActivity : ComponentActivity() {
-    @RequiresPermission(Manifest.permission.POST_NOTIFICATIONS)
+    @SuppressLint("MissingPermission")
+    @OptIn(ExperimentalPermissionsApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ZephrSampleClientAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    AppDetails()
-                }
+                LocationTrackingScreen(
+                    onStart = { startLocationUpdates() },
+                    onStop = { stopLocationUpdates() }
+                )
             }
         }
-        checkLocationPermission()
+    }
+
+    val zephrListener = object : ZephrTypes.ZephrEventListener {
+        override fun onZephrLocationChanged(zephrLocationEvent: ZephrTypes.ZephrLocationEvent) {
+            val status = zephrLocationEvent.status
+            val location = zephrLocationEvent.location
+            if (location != null) {
+                Log.d(
+                    TAG,
+                    "GNSS Update - Status: $status, Lat: ${location.latitude}, Lng: ${location.longitude}, Alt: ${location.altitude}"
+                )
+            } else {
+                Log.d(TAG, "GNSS Update - Status: $status, Location: null")
+            }
+        }
+
+        override fun onPoseChanged(
+            zephrPoseEvent: ZephrTypes.ZephrPoseEvent
+        ) {
+            Log.d(
+                TAG,
+                "Pose Update - yaw: ${zephrPoseEvent.yprWithTimestamp?.first?.get(0)} pitch: ${
+                    zephrPoseEvent.yprWithTimestamp?.first?.get(
+                        1
+                    )
+                } roll: ${zephrPoseEvent.yprWithTimestamp?.first?.get(2)}"
+            )
+        }
     }
 
     override fun onDestroy() {
@@ -46,93 +76,60 @@ class MainActivity : ComponentActivity() {
         super.onDestroy()
     }
 
-    @RequiresPermission(Manifest.permission.POST_NOTIFICATIONS)
-    private fun checkLocationPermission() {
-        if (ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED
-        ) {
-            // Permission given, initialize ZephrRealtimeSDK
-            startLocationUpdates()
-        } else {
-            // Permission NOT given (yet)
-            registerForActivityResult(ActivityResultContracts.RequestPermission()) { permission ->
-                if (permission) {
-                    // User granted, initialize ZephrRealtimeSDK
-                    startLocationUpdates()
-                } else {
-                    // User Declined
-                }
-            }.launch(Manifest.permission.ACCESS_FINE_LOCATION)
-        }
-    }
-
-    @RequiresPermission(allOf = [Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.POST_NOTIFICATIONS])
     private fun startLocationUpdates() {
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.POST_NOTIFICATIONS
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return
+        try {
+            ZephrLocationManager.start(this)
+            ZephrLocationManager.requestLocationUpdates(zephrListener)
+        } catch ( e: SecurityException) {
+            Log.e(TAG, "Permissions not granted, error: ${e.message}")
         }
-        ZephrLocationManager.start(this)
-        ZephrLocationManager.requestLocationUpdates(object : ZephrTypes.ZephrEventListener {
-            override fun onZephrLocationChanged(zephrLocationEvent: ZephrTypes.ZephrLocationEvent) {
-                val status = zephrLocationEvent.status
-                val location = zephrLocationEvent.location
-                if (location != null) {
-                    Log.d(
-                        TAG,
-                        "GNSS Update - Status: $status, Lat: ${location.latitude}, Lng: ${location.longitude}, Alt: ${location.altitude}"
-                    )
-                } else {
-                    Log.d(TAG, "GNSS Update - Status: $status, Location: null")
-                }
-            }
+    }
 
-            override fun onPoseChanged(
-                zephrPoseEvent: ZephrTypes.ZephrPoseEvent
-            ) {
-                Log.d(
-                    TAG,
-                    "Pose Update - yaw: ${zephrPoseEvent.yprWithTimestamp?.first?.get(0)} pitch: ${zephrPoseEvent.yprWithTimestamp?.first?.get(1)} roll: ${zephrPoseEvent.yprWithTimestamp?.first?.get(2)}"
-                )
-            }
-        })
+    private fun stopLocationUpdates() {
+        ZephrLocationManager.removeLocationUpdates(zephrListener)
+        ZephrLocationManager.stop(this)
+        Log.d(TAG, "Zephr updates stopped.")
     }
 }
 
-private const val TAG = "ZephrSampleClientApp"
-
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
-fun AppDetails(modifier: Modifier = Modifier) {
-    Text(
-        text = "Zephr Location SDK sample app",
-        modifier = modifier
-            .fillMaxSize()
-            .wrapContentSize(Alignment.Center)
+fun LocationTrackingScreen(onStart: () -> Unit, onStop: () -> Unit) {
+    val permissionState = rememberMultiplePermissionsState(
+        permissions = mutableListOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ).apply {
+            // POST_NOTIFICATIONS only exists on API 33+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                add(Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }
     )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun AppDetailsPreview() {
-    ZephrSampleClientAppTheme {
-        AppDetails()
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        if (permissionState.allPermissionsGranted) {
+            // Screen when permissions are granted
+            Text("Location Permissions Granted")
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(onClick = onStart) { Text("Start Zephr Service") }
+            Button(onClick = onStop) { Text("Stop Zephr Service") }
+        } else {
+            // Permission Denied / Initial Screen
+            val textToShow = if (permissionState.shouldShowRationale) {
+                "Location and Notification access is needed to start Zephr location service."
+            } else {
+                "This feature requires Location and Notification permissions."
+            }
+
+            Text(textToShow, textAlign = TextAlign.Center, modifier = Modifier.padding(16.dp))
+            Button(onClick = { permissionState.launchMultiplePermissionRequest() }) {
+                Text("Grant Permissions")
+            }
+        }
     }
 }
